47140c2c515d1a22f405e039ec3ef7283f4b0f4b
RAKESH <RAKESH@RAKESH-PC>
2017-06-04 10:33:46 +0530

    version 1.10

---
+++ src/TestGit.java
@@ -0,0 +1,15 @@
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
